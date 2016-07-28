492a00ee2d77bb78f795944b5e497651f9bced84
AdamTomaja <adiscool@tlen.pl>
2016-07-28 23:52:03 +0200

    Views for displaying Questions and Choices

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
+from .models import  Question
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    return HttpResponse("Hello, World! You are at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse("You`re looking at the results of question %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You`re voting on question %s" % question_id)
