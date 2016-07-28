4ba2d5e95cd119ad5f9126f1f24cea595505bd53
AdamTomaja <adiscool@tlen.pl>
2016-07-29 00:10:14 +0200

    Voting and results list

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse,Http404
-from .models import  Question
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
-
+from django.core.urlresolvers import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -19,8 +19,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse("You`re looking at the results of question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You didn`t selected a choice'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id)))
     return HttpResponse("You`re voting on question %s" % question_id)
